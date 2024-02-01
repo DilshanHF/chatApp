d4f7cb732616e608809c1271620dc6d1204dd975
DILSHAN <dilshanfonseka76@gmail.com>
2024-02-01 13:28:52 +0530

    All done

--- src/main/java/lk/ijse/GroupchatApplication/controller/ClientRoomController.java
+++ src/main/java/lk/ijse/GroupchatApplication/controller/ClientRoomController.java
@@ -1,19 +1,52 @@
 package lk.ijse.GroupchatApplication.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
 import lk.ijse.GroupchatApplication.client.Client;
 
-public class ClientRoomController {
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.ResourceBundle;
+
+public class ClientRoomController implements Initializable {
+    private final String[] emojis = {
+            "\uD83D\uDE00", // 😀
+            "\uD83D\uDE01", // 😁
+            "\uD83D\uDE02", // 😂
+            "\uD83D\uDE03", // 🤣
+            "\uD83D\uDE04", // 😄
+            "\uD83D\uDE05", // 😅
+            "\uD83D\uDE06", // 😆
+            "\uD83D\uDE07", // 😇
+            "\uD83D\uDE08", // 😈
+            "\uD83D\uDE09", // 😉
+            "\uD83D\uDE0A", // 😊
+            "\uD83D\uDE0B", // 😋
+            "\uD83D\uDE0C", // 😌
+            "\uD83D\uDE0D", // 😍
+            "\uD83D\uDE0E", // 😎
+            "\uD83D\uDE0F", // 😏
+            "\uD83D\uDE10", // 😐
+            "\uD83D\uDE11", // 😑
+            "\uD83D\uDE12", // 😒
+            "\uD83D\uDE13"  // 😓
+    };
 
     @FXML
     private AnchorPane emojiAnchorpane;
@@ -49,23 +82,83 @@ public class ClientRoomController {
     private VBox vBox;
     @FXML
     private TextField txtMsgArea;
+    private Client client;
 
     public static void leaveChat() {
+
     }
 
     @FXML
     void btnEmojiOnAction(ActionEvent event) {
-
+        emojiAnchorpane.setVisible(!emojiAnchorpane.isVisible());
     }
 
     @FXML
     void btnImageOnClick(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Image File");
+        FileChooser.ExtensionFilter imageFilter = new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg");
+        fileChooser.getExtensionFilters().add(imageFilter);
+        File selectedFile = fileChooser.showOpenDialog(new Stage());
+        if (selectedFile != null) {
+            try {
+                byte[] bytes = Files.readAllBytes(selectedFile.toPath());
+                HBox hBox = new HBox();
+                hBox.setStyle("-fx-fill-height: true; -fx-min-height: 50; -fx-pref-width: 520; -fx-max-width: 520; -fx-padding: 10; -fx-alignment: center-right;");
+
+                // Display the image in an ImageView or any other UI component
+                ImageView imageView = new ImageView(new Image(new FileInputStream(selectedFile)));
+                imageView.setStyle("-fx-padding: 10px;");
+                imageView.setFitHeight(180);
+                imageView.setFitWidth(100);
+
+                hBox.getChildren().addAll(imageView);
+                vBox.getChildren().add(hBox);
+
+                client.sendImage(bytes);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
     }
 
     @FXML
     void btnOnSendMsg(ActionEvent event) {
+        String text = txtMsgArea.getText();
+        if (text != null){
+            appendText(text);
+            try {
+                client.sendMessage(text);
+                txtMsgArea.clear();
+            } catch (IOException e) {
+                new Alert(Alert.AlertType.ERROR, "Something went wrong : server down").show();
+            }
+        } else{
+            new Alert(Alert.AlertType.INFORMATION, "message is empty").show();
+        }
 
+    }
+
+     void appendText(String massage) {
+         HBox hBox = new HBox();
+         hBox.setStyle("-fx-alignment: center-right;-fx-fill-height: true;-fx-min-height: 50;-fx-pref-width: 520;-fx-max-width: 520;-fx-padding: 10");
+         Label massageLbl = new Label(massage);
+         massageLbl.setStyle("-fx-background-color:  #008011;-fx-background-radius:15;-fx-font-size: 18;-fx-font-weight: normal;-fx-text-fill: white;-fx-wrap-text: true;-fx-alignment: center-left;-fx-content-display: left;-fx-padding: 10;-fx-max-width: 350;");
+         hBox.getChildren().add(massageLbl);
+         vBox.getChildren().add(hBox);
+         new Thread(() -> {
+             playSound("/media/messageSend.mp3");
+         }).start();
+
+    }
+
+    private void playSound(String sound) {
+        try {
+            new Player(new BufferedInputStream(getClass().getClassLoader().getResourceAsStream(sound))).play();
+        } catch (JavaLayerException e) {
+            new Alert(Alert.AlertType.ERROR, "Audio not available").show();
+        }
     }
 
     @FXML
@@ -74,11 +167,73 @@ public class ClientRoomController {
     }
 
     public void writeMessage(String message) {
+        HBox hBox = new HBox();
+        hBox.setStyle("-fx-alignment: center-left;-fx-fill-height: true;-fx-min-height: 50;-fx-pref-width: 520;-fx-max-width: 520;-fx-padding: 10");
+        Label messageLbl = new Label(message);
+        messageLbl.setStyle("-fx-background-color:   #2980b9;-fx-background-radius:15;-fx-font-size: 18;-fx-font-weight: normal;-fx-text-fill: white;-fx-wrap-text: true;-fx-alignment: center-left;-fx-content-display: left;-fx-padding: 10;-fx-max-width: 350;");
+        hBox.getChildren().add(messageLbl);
+        Platform.runLater(() -> {
+
+            vBox.getChildren().add(hBox);
+        });
     }
 
     public void setClient(Client client) {
+        this.client = client;
     }
 
-    public void setImage(byte[] bytes, String utf) {
+    public void setImage(byte[] bytes, String sender) {
+        HBox hBox = new HBox();
+        Label messageLbl = new Label(sender);
+        messageLbl.setStyle("-fx-background-color:   #2980b9;-fx-background-radius:15;-fx-font-size: 18;-fx-font-weight: normal;-fx-text-fill: white;-fx-wrap-text: true;-fx-alignment: center;-fx-content-display: left;-fx-padding: 10;-fx-max-width: 350;");
+
+        hBox.setStyle("-fx-fill-height: true; -fx-min-height: 50; -fx-pref-width: 520; -fx-max-width: 520; -fx-padding: 10; " + (sender.equals(client.getName()) ? "-fx-alignment: center-right;" : "-fx-alignment: center-left;"));
+        // Display the image in an ImageView or any other UI component
+        Platform.runLater(() -> {
+            ImageView imageView = new ImageView(new Image(new ByteArrayInputStream(bytes)));
+            imageView.setStyle("-fx-padding: 10px;");
+            imageView.setFitHeight(180);
+            imageView.setFitWidth(100);
+
+            hBox.getChildren().addAll(messageLbl, imageView);
+            vBox.getChildren().add(hBox);
+
+        });
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        emojiAnchorpane.setVisible(false);
+        int buttonIndex = 0;
+        for (int row = 0; row < 4; row++){
+            for(int column =0; column <4 ; column++){
+                if (buttonIndex < emojis.length){
+                    String emoji = emojis[buttonIndex];
+                    JFXButton emojiButton = createEmojiButton(emoji);
+                    emojiGridpane.add(emojiButton,column,row);
+                    buttonIndex++;
+                }else{
+                    break;
+                }
+            }
+        }
+
+    }
+
+    private JFXButton createEmojiButton(String emoji) {
+        JFXButton button = new JFXButton(emoji);
+        button.getStyleClass().add("emoji - button");
+        button.setOnAction(this::emojiButtoOnAction);
+        button.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        GridPane.setFillWidth(button, true);
+        GridPane.setFillHeight(button, true);
+        button.setStyle("-fx-font-size: 15; -fx-text-fill: black; -fx-background-color: #F0F0F0; -fx-border-radius: 50");
+        return button;
+    }
+
+    private void emojiButtoOnAction(ActionEvent event) {
+        JFXButton button = (JFXButton) event.getSource();
+        txtMsgArea.appendText(button.getText());
     }
 }
